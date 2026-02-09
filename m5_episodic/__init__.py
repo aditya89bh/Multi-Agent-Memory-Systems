a75d3df92794c7d926ee09bfb09c78baf4987089
Aditya bhatia <aditya89bh@gmail.com>
2026-02-09 10:31:53 +0530

    Create __init__.py

---
+++ m5_episodic/__init__.py
@@ -0,0 +1,18 @@
+"""
+M5 – Team Episodic Memory
+
+This module packages coordinated multi-agent work into replayable episodes.
+
+Public API:
+- Episode
+- EpisodeEvent
+- EpisodeBuilder
+- EpisodeStore
+"""
+
+from .episode import (
+    Episode,
+    EpisodeEvent,
+    EpisodeBuilder,
+    EpisodeStore,
+)
